192460ef2d1e2e19e9f5ad19a5701369aa9c4dc5
David Schurer <david.schurer@yahoo.com>
2024-04-20 19:58:56 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <NavigationBar />
       <Routes>
-        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/things-to-read" element={<ThingsToReadPage />} />
       </Routes>
     </Router>
