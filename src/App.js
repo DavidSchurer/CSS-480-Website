431dcd25eed9ba77c1e023878d5d5a5050ba41a8
David Schurer <david.schurer@yahoo.com>
2024-05-21 19:47:58 -0700

    updated router to dislay interests page upon clicking website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './Home';
 import ThingsToReadPage from './ThingsToRead';
 import NavigationBar from './NavigationBar';
@@ -10,7 +10,7 @@ function App() {
     <Router basename = "/CSS-480-Website">
       <NavigationBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/interests-page" />} />
         <Route path="/things-to-read" element={<ThingsToReadPage />} />
         <Route path="/interests-page" element={<InterestsPage />} />
       </Routes>
